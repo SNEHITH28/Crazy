132687e726c0377ae24ace9497c265df17625c6c
cheu <ravikiranf6@gmail.com>
2021-01-13 23:57:11 +0530

    Crazy commit

---
+++ Crazy/src/Kiki/CrazyOne.java
@@ -0,0 +1,9 @@
+package Kiki;
+
+public class CrazyOne {
+
+	public static void main(String[] args) {
+		System.out.println("hi");
+	}
+
+}
---
+++ Crazy/src/Kiki/CrazyTwo.java
@@ -0,0 +1,11 @@
+package Kiki;
+
+public class CrazyTwo {
+
+	public static void main(String[] args) {
+		
+		System.out.println("Second hi");
+
+	}
+
+}
